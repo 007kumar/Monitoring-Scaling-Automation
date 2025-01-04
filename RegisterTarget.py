1b202e47a1ec8572e88ae506edbc6fa7c4df48dc
007kumar <shiv.rv007@gmail.com>
2025-01-04 20:26:04 +0530

    Update RegisterTarget.py
    code modified

--- RegisterTarget.py
+++ RegisterTarget.py
@@ -7,14 +7,14 @@ response = elb.create_target_group(
     Name='KumarB8TG',
     Protocol='HTTP',
     Port=80,
-    VpcId='vpc-09f02049d6176fe30',  # Replace with your VPC ID
+    VpcId='vpc-09f02049d6176fe30', 
     TargetType='instance'
 )
 
 target_group_arn = response['TargetGroups'][0]['TargetGroupArn']
 print(f"Target Group Created with ARN: {target_group_arn}")
 
-instance_id = 'i-066a8410f3b9d7f66'  # Replace with the EC2 instance ID you want to register
+instance_id = 'i-066a8410f3b9d7f66'  
 response = elb.register_targets(
     TargetGroupArn=target_group_arn,
     Targets=[{'Id': instance_id}]
