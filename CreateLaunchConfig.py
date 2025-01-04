f410dc9b785093851c6bf427cff8574b9e985a77
007kumar <shiv.rv007@gmail.com>
2025-01-04 20:24:42 +0530

    Update CreateLaunchConfig.py
    code modified

--- CreateLaunchConfig.py
+++ CreateLaunchConfig.py
@@ -5,12 +5,12 @@ ec2 = boto3.client('autoscaling')
 
 # Create Launch Configuration
 response = ec2.create_launch_configuration(
-    LaunchConfigurationName='KumarLaunchConfig',  # Name of your Launch Configuration
-    ImageId='ami-0e2c8caa4b6378d8c',  # AMI ID (use a valid AMI ID for your region)
-    InstanceType='t2.micro',  # Instance type
-    KeyName='jenki',  # Name of the EC2 Key Pair
-    SecurityGroups=['sg-01f41ec5b97d3998c'],  # List of security group IDs
-    AssociatePublicIpAddress=True  # Optional: Enable public IP assignment
+    LaunchConfigurationName='KumarLaunchConfig', 
+    ImageId='ami-0e2c8caa4b6378d8c',  
+    InstanceType='t2.micro', 
+    KeyName='jenki', 
+    SecurityGroups=['sg-01f41ec5b97d3998c'], 
+    AssociatePublicIpAddress=True 
 )
 
 print('Launch Configuration Created:', response)
